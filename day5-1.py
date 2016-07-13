1a32b7acd4e69cca43d94fc1a77e102989af04b1
Tessa Karlovitz <tessa.marie.karlovitz@temple.edu>
2016-07-13 14:37:25 -0400

    Submit Day 5 Part 1

--- day5-1.py
+++ day5-1.py
@@ -1,3 +1,21 @@
+import re
+
+nice_strings = []
 
 with open('day5input.txt', 'r') as f:
-    day_5_input = f.read()
+    for line in f:
+        line = line.strip()
+
+        bad_pairs = re.search(r"(ab|cd|pq|xy)", line)
+        if bad_pairs != None:
+            continue
+        vowels = re.findall(r"[aeiou]", line)
+        if vowels != None:
+            if len(vowels) < 3:
+                continue
+        double = re.search(r"(.)\1", line)
+        if double == None:
+            continue
+        nice_strings.append(line)
+
+    print(len(nice_strings))
